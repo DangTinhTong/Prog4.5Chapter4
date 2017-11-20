62fb5cfdeec5ef2be93d9592c541c2a5ccce0caa
admin <dangtinhtong@gmail.com>
2017-11-20 12:16:00 +0700

    First commit

--- Prog4.5/main.m
+++ Prog4.5/main.m
@@ -2,9 +2,11 @@
 //  main.m
 //  Prog4.5
 //
-//  Created by admin on 11/19/17.
-//  Copyright © 2017 admin. All rights reserved.
-//
+//  Created by Tống Đăng Tình on 11/19/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
+//-----------------------------------------
+// Basic conversitions in Objective-C
+
 
 #import <Foundation/Foundation.h>
 
@@ -12,6 +14,19 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        float f1=123.125;
+        int   i1, i2=-150;
+        
+        i1=f1; // floating to integer conversation
+        NSLog(@"%f assigned to an int produces %i", f1, i1);
+        
+        f1=i2;  //integer to floating conversation
+        NSLog(@"%i assigned to a float produces %f", i2, f1);
+        
+        
+        f1=i2/10; // integer divided by integer
+        NSLog(@"%i diveded by 10 produces %f", i2, f1);
     }
     return 0;
 }
